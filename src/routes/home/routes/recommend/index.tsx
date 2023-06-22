9478639b47e245d020819929b4ef8b4bf7af26e3
lansen <bluewoods.lan@gmail.com>
2023-06-22 22:03:33 +0800

    feat: 移除 react-vant

--- src/routes/home/routes/recommend/index.tsx
+++ src/routes/home/routes/recommend/index.tsx
@@ -1,19 +1,11 @@
-import React, { ElementRef, useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, { ElementRef, useRef, useState } from "react";
 import styles from "./styles.module.scss";
 import Slider from "@/components/Slider";
 import RecommendList from "@/components/recommendList";
 import Scroll from "@/components/Scroll";
-import { PullRefresh, Toast } from "react-vant";
 import useMount from "@/hooks/useMount";
 import { getBannerListData, getRecommendListData } from "@/api/request";
 import Loading from "@/baseUI/Loading";
-
-const recommendList = Array.from({ length: 9 }, (_, index) => ({
-  id: index + 1,
-  picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
-  playCount: 172122,
-  name: "朴树、许巍、李健、郑钧、老狼、赵雷",
-}));
 
 const Recommend: React.FC = () => {
   const [bannerList, setBannerList] = useState([]);
@@ -36,18 +28,6 @@ const Recommend: React.FC = () => {
     const res = await getRecommendListData();
     const data = res as any;
     setRecommendList(data.result);
-  };
-
-  const onRefresh = (showToast: boolean) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        if (showToast) {
-          Toast.info("刷新成功");
-        }
-
-        resolve(true);
-      }, 1000);
-    });
   };
 
   return (
