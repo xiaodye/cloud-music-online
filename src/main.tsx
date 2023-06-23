81d34a50a78c5660be6c381a9a7c055930e22c21
lansen <bluewoods.lan@gmail.com>
2023-06-24 02:26:51 +0800

    feat: 使用 css var

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 import "@/styles/theme.css";
+import "@/styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
