e18bdbd26f1383198cdefd558a3bd1ea19d8d562
lansen <bluewoods.lan@gmail.com>
2023-06-24 02:42:27 +0800

    refactor: 使用 css var 重构 scss var

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
-import "@/styles/theme.css";
+import "@/styles/theme.css.ts";
 import "@/styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
