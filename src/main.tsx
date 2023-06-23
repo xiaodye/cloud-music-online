e52bbcc75b56d922ad132972999e7bd728075c04
lansen <bluewoods.lan@gmail.com>
2023-06-24 02:50:14 +0800

    feat: 使用 css 重构 全局 scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.scss";
+import "./index.css";
 import "@/styles/theme.css.ts";
-import "@/styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
