865c979e62ca1ec595b8a6dfe412420a4ea30729
lansen <bluewoods.lan@gmail.com>
2023-08-12 23:22:05 +0800

    feat: 使用 rollup-plugin-visualizer 分析打包产物

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    visualizer({
+      // 打包完成后自动打开浏览器，显示产物体积报告
+      open: true,
+    }),
+  ],
   css: {
     modules: {
       localsConvention: "camelCaseOnly", // 修改生成的配置对象的key的展示形式(驼峰还是中划线形式)
