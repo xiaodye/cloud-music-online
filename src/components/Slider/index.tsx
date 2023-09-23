22bf6a16a0d062a97c5c3e4b588d99fb029b84ed
lansen <bluewoods.lan@gmail.com>
2023-09-23 22:12:44 +0800

    feat: Swiper@9.6.1 ->Swiper@10.3.0

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,11 +1,10 @@
 import styles from "./styles.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
+import { Pagination } from "swiper/modules";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import placeImg from "@/assets/music.png";
 
-import "swiper/css";
-import "swiper/css/pagination";
+import "swiper/swiper-bundle.css";
 
 type IProps = {
   bannerList: { imageUrl: string }[];
